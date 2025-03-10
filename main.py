d0c0d07e731c20a05fda7a473716a39131f49309
Teerawat P. <teewat@yahoo.com>
2025-03-10 14:12:37 +1100

    add error handling

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ def get_book_test(file_path):
         content = f.read()
         return content
 def get_report(file_path):
+    book_content = get_book_test(file_path)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {file_path}...")
     print("----------- Word Count ----------")
-    print(f"Found {get_num_words(get_book_test(file_path))} total words")
+    print(f"Found {get_num_words(book_content)} total words")
     print("--------- Character Count -------")
-    print_result(sorted_char(get_num_characters(get_book_test(file_path))))
+    print_result(sorted_char(get_num_characters(book_content)))
     print("============= END ===============")
     
 def main():
@@ -19,6 +20,9 @@ def main():
         print("Usage: python3 main.py <path_to_book>")
         exit(1)
     if len(argv) == 2:
-      get_report(argv[1])
+      try:
+        get_report(argv[1])
+      except Exception as e:
+        print(f"Error: {e}")
 
 main()
